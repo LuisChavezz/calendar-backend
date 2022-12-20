12aceba55478c40328aab881b29b9b160b0d657d
unknown <ing.luischavez.dev>
2022-12-19 19:03:30 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config(); // paquete necesario para usar 'process.env'
+
+
+// Crear servidor de express
+const app = express();
+
+// Directorio público
+app.use( express.static('public') )
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'))
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto: ${process.env.PORT}`);
+})
