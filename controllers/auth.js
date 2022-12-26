b85f8509cf827ddebae21db7b4f402086c2d21ba
unknown <ing.luischavez.dev>
2022-12-25 20:29:07 -0600

    Manejo del body y status

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,30 @@
 const { response } = require('express')
 
-const createUser = (_, res = response) => {
+const createUser = ( req, res = response ) => {
+  const { name } = req.body;
+
+  if ( name.length < 4 ) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe ser de al menos 4 letras'
+    })
+  }
+
   res.json({
     ok: true,
-    msg: 'register'
+    msg: 'register',
+    body: req.body
   })
 }
 
-const userLogin = (_, res = response) => {
+const userLogin = ( _, res = response ) => {
   res.json({
     ok: true,
     msg: 'login'
   })
 }
 
-const renewToken = (_, res) => {
+const renewToken = ( _, res = response ) => {
   res.json({
     ok: true,
     msg: 'renew'
--- index.js
+++ index.js
@@ -6,10 +6,13 @@ require('dotenv').config(); // paquete necesario para usar 'process.env'
 const app = express();
 
 // Directorio público
-app.use( express.static('public') )
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Rutas
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'));
 
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {
