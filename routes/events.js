3cde56b2db004dd4592f9c33405190f271191078
unknown <ing.luischavez.dev>
2023-02-03 22:08:00 -0600

    Validaciones de la creación de eventos

--- routes/events.js
+++ routes/events.js
@@ -1,22 +1,38 @@
-/*
+ /*
     Rutas de Eventos (events)
     url: host + /api/events
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator')
 const router = Router();
 
 // Métodos
+const { validateFields } = require('../middlewares/fields-validator');
 const { validateJWT } = require('../middlewares/validate-jwt');  // validación del JWT
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 // Validación del JWT
 router.use( validateJWT ) // * Cualquier petición a continuación requerirá la validación del token
 
 // Get events
 router.get('/', getEvents)
-router.post('/', createEvent)
+
+router.post(
+  '/',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+    check('end', 'Fecha de fin es obligatoria').custom( isDate ),
+    // check('start', 'Fecha de inicio es obligatoria').,
+    validateFields
+  ],
+  createEvent
+)
+
 router.put('/', updateEvent)
+
 router.delete('/', deleteEvent)
 
 
